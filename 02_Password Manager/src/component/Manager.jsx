be9a7388a6f15413aa684e9ad9be713b52606ce7
Parth Surnar <parth.surnar6@gmail.com>
2025-01-26 18:31:23 +0530

    added local storage function

--- 02_Password Manager/src/component/Manager.jsx
+++ 02_Password Manager/src/component/Manager.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import { useRef } from 'react'
+import React, { useEffect } from 'react'
+import { useRef, useState } from 'react'
 
 const Manager = () => {
     const ref = useRef()
+    const [form, setform] = useState({ site: "", username: "", password: "" })
+    const [passwordArray, setPasswordArray] = useState([])
+
+    useEffect(() => {
+        let passwords = localStorage.getItem("passwords")
+        if (passwords) {
+            setPasswordArray(JSON.parse(passwords))
+        }
+    }, [])
+
+
+
+
     const showPassword = () => {
         // alert("show the password")
         if (ref.current.src.includes("eyecross.png")) {
@@ -10,6 +23,16 @@ const Manager = () => {
         } else {
             ref.current.src = "eyecross.png"
         }
+    }
+
+    const savePassword = () => {
+        setPasswordArray([...passwordArray, form])
+        localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
+        console.log([...passwordArray, form])
+    }
+
+    const handleChange = (e) => {
+        setform({ ...form, [e.target.name]: e.target.value })
     }
 
 
@@ -28,19 +51,19 @@ const Manager = () => {
                 <p className='font-Montserrat text-center text-lg pt-2'>Your own Password Manager</p>
                 <div className="text-white p-4 flex flex-col gap-5 items-center">
 
-                    <input placeholder='Enter Website URL' className='rounded-full border border-[#B39DDB] text-black focus:outline-[#9932CC] p-4 py-1 w-full' type="text" name="" id="" />
+                    <input value={form.site} onChange={handleChange} placeholder='Enter Website URL' className='rounded-full border border-[#B39DDB] text-black focus:outline-[#9932CC] p-4 py-1 w-full' type="text" name="site" id="" />
 
                     <div className="flex w-full gap-3 justify-between ">
-                        <input placeholder='Enter Username' className='rounded-full border border-[#B39DDB] text-black focus:outline-[#9932CC] flex-1 p-4 py-1' type="text" name="" id="" />
+                        <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-[#B39DDB] text-black focus:outline-[#9932CC] flex-1 p-4 py-1' type="text" name="username" id="" />
 
                         <div className="relative items-center text-center flex">
-                            <input placeholder='Enter Password' className='rounded-full border border-[#B39DDB] text-black focus:outline-[#9932CC] flex-1 p-4 py-1' type="text" name="" id="" />
+                            <input value={form.password} onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-[#B39DDB] text-black focus:outline-[#9932CC] flex-1 p-4 py-1' type="text" name="password" id="" />
                             <span className='absolute right-2 text-black items-center text-center cursor-pointer' onClick={showPassword}>
                                 <img ref={ref} width={20} src="eye.png" alt="" />
                             </span>
                         </div>
                     </div>
-                    <button className='text-black cursor-pointer font-Montserrat justify-center items-center flex w-fit bg-purple-400 rounded-full px-3 py-2 hover:bg-purple-300 transition-all duration-200 ease-in-out font-medium gap-1 border border-purple-600 '>
+                    <button onClick={savePassword} className='text-black cursor-pointer font-Montserrat justify-center items-center flex w-fit bg-purple-400 rounded-full px-3 py-2 hover:bg-purple-300 transition-all duration-200 ease-in-out font-medium gap-1 border border-purple-600 '>
                         <lord-icon
                             src="https://cdn.lordicon.com/jgnvfzqg.json"
                             trigger="hover">
