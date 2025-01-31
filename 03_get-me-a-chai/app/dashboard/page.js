d20dfd9036dade5876d00764b689ffb3da9130bb
Parth Surnar <parth.surnar6@gmail.com>
2025-01-31 14:54:28 +0530

    Done with GET ME A CHAI project all ui and functions will work on payment integration later after fixing issues with razorpay

--- 03_get-me-a-chai/app/dashboard/page.js
+++ 03_get-me-a-chai/app/dashboard/page.js
@@ -84,12 +84,6 @@ const Dashboard = () => {
                         <input value={form.profilepic ? form.profilepic : ""} onChange={handleChange} type="text" name='profilepic' id="profilepic" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                     </div>
 
-                    {/* input for cover pic  */}
-                    <div className="my-2">
-                        <label htmlFor="coverpic" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cover Picture</label>
-                        <input value={form.coverpic ? form.coverpic : ""} onChange={handleChange} type="text" name='coverpic' id="coverpic" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    </div>
-
                     {/* input razorpay id */}
                     <div className="my-2">
                         <label htmlFor="razorpayid" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Razorpay Id</label>
