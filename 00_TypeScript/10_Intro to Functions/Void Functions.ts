b9902033a214d3bd6e311b41df4df9aa511a31f0
Parth Surnar <parth.surnar6@gmail.com>
2025-02-06 17:07:32 +0530

    Void Functions in TypeScript

---
+++ 00_TypeScript/10_Intro to Functions/Void Functions.ts
@@ -0,0 +1,11 @@
+// ===================================================
+// 🔹 3️⃣ VOID FUNCTIONS (NO RETURN VALUE)
+// ===================================================
+// `void` is used when a function does not return anything.
+
+function logMessage(message: string): void {
+    console.log(message);
+  }
+  
+  logMessage("This function returns nothing!"); // ✅ Output: This function returns nothing!
+  
