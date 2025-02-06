d12d73dea2813aa120777fc795aabc6cb67fa369
Parth Surnar <parth.surnar6@gmail.com>
2025-02-06 17:11:05 +0530

    Arrow Functions in TypeScript

---
+++ 00_TypeScript/10_Intro to Functions/Arrow Functions.ts
@@ -0,0 +1,8 @@
+// ===================================================
+// 🔹 8️⃣ ARROW FUNCTIONS (=>)
+// ===================================================
+// Arrow functions provide a shorter syntax.
+
+const square = (num: number): number => num * num;
+
+console.log(square(4)); // ✅ Output: 16
