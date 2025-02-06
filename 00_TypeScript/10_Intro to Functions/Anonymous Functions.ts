403f85f7326a9a78d98e378724c9feef99cc3420
Parth Surnar <parth.surnar6@gmail.com>
2025-02-06 17:11:35 +0530

    Anonymous Functions in TypeScript

---
+++ 00_TypeScript/10_Intro to Functions/Anonymous Functions.ts
@@ -0,0 +1,11 @@
+// ===================================================
+// 🔹 9️⃣ ANONYMOUS FUNCTIONS
+// ===================================================
+// Functions without a name, often used as callbacks.
+
+const greetUser = function (name: string): string {
+    return `Hello, ${name}!`;
+  };
+  
+  console.log(greetUser("Parth")); // ✅ Output: Hello, Parth!
+  
