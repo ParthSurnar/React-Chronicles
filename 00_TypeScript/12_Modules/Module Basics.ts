536fd103e3ccdc010f5d3c3cd90b1f3e7e1c72a9
Parth Surnar <parth.surnar6@gmail.com>
2025-02-07 19:44:51 +0530

    Module Basics

---
+++ 00_TypeScript/12_Modules/Module Basics.ts
@@ -0,0 +1,8 @@
+/*
+📌 **TypeScript Modules - Overview**
+--------------------------------------
+✅ Modules help in organizing code by splitting it into different files.
+✅ Each file in TypeScript is **automatically a module** when it has an `export` or `import`.
+✅ `export` makes a variable, function, or class available for other files.
+✅ `import` brings the exported module into another file.
+*/
