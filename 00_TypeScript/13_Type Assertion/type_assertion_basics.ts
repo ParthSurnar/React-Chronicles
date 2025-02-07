84ef74d3415943b6e2eb592cce4bc9d37f99ddf2
Parth Surnar <parth.surnar6@gmail.com>
2025-02-07 19:51:44 +0530

    Type Assertion Basics

---
+++ 00_TypeScript/13_Type Assertion/type_assertion_basics.ts
@@ -0,0 +1,9 @@
+/*
+📌 **Type Assertion - Overview**
+--------------------------------------
+✅ Type Assertion tells TypeScript to **treat a value as a specific type**.
+✅ Used when TypeScript can't automatically infer the correct type.
+✅ Syntax:
+   1️⃣ `value as Type`
+   2️⃣ `<Type>value` (Not recommended in JSX/TSX)
+*/
