4a781abadd748aa2172376d765180e4f2e38fa4b
Parth Surnar <parth.surnar6@gmail.com>
2025-02-08 23:13:22 +0530

    Component Custom Hooks

---
+++ 00_TypeScript/00_React TypeScript/component_custom_hooks.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+/* 📌 Custom Hook to Fetch Data */
+const useFetch = <T,>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { data, loading };
+};
+
+export default useFetch;
