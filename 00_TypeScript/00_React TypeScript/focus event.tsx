f54fb58e2733dfebe788419e072930ddd7e591b1
Parth Surnar <parth.surnar6@gmail.com>
2025-02-09 19:06:53 +0530

    Focus Event

---
+++ 00_TypeScript/00_React TypeScript/focus event.tsx
@@ -0,0 +1,8 @@
+const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    console.log("Element focused");
+  };
+  
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    console.log("Element lost focus");
+  };
+  
