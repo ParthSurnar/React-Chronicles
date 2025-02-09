d1df6053708036587e31ea741d4034f2bbfb742e
Parth Surnar <parth.surnar6@gmail.com>
2025-02-09 19:06:10 +0530

    Media Event

---
+++ 00_TypeScript/00_React TypeScript/media event.tsx
@@ -0,0 +1,8 @@
+const handlePlay = (event: React.SyntheticEvent<HTMLVideoElement>) => {
+    console.log("Video playing");
+  };
+  
+  const handlePause = (event: React.SyntheticEvent<HTMLVideoElement>) => {
+    console.log("Video paused");
+  };
+  
