8bc2abfb0a1f3e2818dc5cd6ab7b38ebdc06d3d5
Parth Surnar <parth.surnar6@gmail.com>
2025-02-09 23:39:01 +0530

    Utility Types

---
+++ 00_TypeScript/00_React TypeScript/Utility types.tsx
@@ -0,0 +1,16 @@
+type User = {
+    id: number;
+    name: string;
+    email: string;
+  };
+  
+  // Make everything optional
+  type OptionalUser = Partial<User>;
+  
+  const updateUser = (user: OptionalUser) => {
+    console.log(user);
+  };
+  
+  updateUser({ name: "Parth" }); // ✅ Allowed because all fields are optional
+    updateUser({}); // ✅ Allowed because all fields are optional
+    updateUser({ name: "Parth", email: " [email protected]" }); // ✅ Allowed because all fields are optional  
