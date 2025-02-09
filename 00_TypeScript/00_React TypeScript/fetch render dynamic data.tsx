38644b7468447c516d512c59d3975e5b688f1953
Parth Surnar <parth.surnar6@gmail.com>
2025-02-09 23:30:23 +0530

    Fetch Render Dynamic Data

---
+++ 00_TypeScript/00_React TypeScript/fetch render dynamic data.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+const fetchProducts = async (): Promise<Product[]> => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  return res.json();
+};
+
+const ProductList = () => {
+  const [products, setProducts] = useState<Product[] | null>(null);
+
+  useEffect(() => {
+    fetchProducts().then(setProducts);
+  }, []);
+
+  if (!products) return <p>Loading...</p>;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.name} - ${product.price}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ProductList;
