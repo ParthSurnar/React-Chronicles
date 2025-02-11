4e5c1c680eba2a4b0aa3b83ea8c69a6b83e40f1b
Parth Surnar <parth.surnar6@gmail.com>
2025-02-11 11:30:06 +0530

    random Word selecting logic

--- 05_hangman-game/src/App.tsx
+++ 05_hangman-game/src/App.tsx
@@ -1,10 +1,13 @@
 // Code for the App component
 import { useState } from "react";
+import words from "./wordList.json";
 import "./App.css";
 
 function App() {
-
-  const [wordToGuess, setWordToGuess] = useState("test");
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  console.log({ wordToGuess });
 
   return (
     <>
