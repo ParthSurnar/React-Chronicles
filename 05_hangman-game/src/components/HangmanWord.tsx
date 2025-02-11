caa3663c8a8c8a413cb052af9e46ae3b6aa314db
Parth Surnar <parth.surnar6@gmail.com>
2025-02-11 13:23:58 +0530

    Hangman Wording done

--- 05_hangman-game/src/components/HangmanWord.tsx
+++ 05_hangman-game/src/components/HangmanWord.tsx
@@ -1,9 +1,15 @@
-
 const HangmanWord = () => {
+  const word = "hello";
+  const guessedLetters = ["h", "e", "l"];
   return (
-    <div>
-      
+    <div className="flex justify-center items-center gap-4 text-8xl font-bold uppercase ">
+      {word.split("").map((letter, index) => (
+        <div key={index} className="border-b-4 border-black  text-center">
+        <span>{guessedLetters.includes(letter) ? letter : "_"} </span>
+        </div>
+      ))}
     </div>
+
   )
 }
 
