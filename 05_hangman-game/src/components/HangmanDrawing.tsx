1a073da08a3ae55aa4da5249a4cf054d23886f86
Parth Surnar <parth.surnar6@gmail.com>
2025-02-11 14:30:08 +0530

    Hangman Drawing

--- 05_hangman-game/src/components/HangmanDrawing.tsx
+++ 05_hangman-game/src/components/HangmanDrawing.tsx
@@ -1,29 +1,30 @@
-const Head =
-  <div className="h-[50px] w-[50px] rounded-full absolute top-[50px] right-[-20px] border-[10px]" />;
-const Body =
-  <div className="h-[100px] w-[10px] border bg-black right-0 top-[100px] absolute" />;
+import React from "react";
 
-const rightArm = <div className="h-[10px] w-[100px] top-[120px] right-[-90px] border bg-black rotate-[-30deg] absolute " />;
+type HangmanDrawingProps = {
+  incorrectGuesses: number; // Track only wrong guesses
+};
 
-const leftArm = <div className="h-[10px] w-[100px] top-[120px] right-[0px] border bg-black rotate-[30deg] absolute " />;
+const bodyParts = [
+ 
+  <div key="head" className="h-[50px] w-[50px] rounded-full border-[10px] absolute top-[50px] right-[-20px]" />, // Head
+  <div key="body" className="h-[100px] w-[10px] bg-black absolute top-[100px] right-0" />, // Body
+  <div key="rightArm" className="h-[10px] w-[100px] bg-black absolute top-[120px] right-[-90px] rotate-[-30deg]" />, // Right Arm
+  <div key="leftArm" className="h-[10px] w-[100px] bg-black absolute top-[120px] right-0 rotate-[30deg]" />, // Left Arm
+  <div key="rightLeg" className="h-[10px] w-[100px] bg-black absolute top-[230px] right-[-70px] rotate-[60deg]" />, // Right Leg
+  <div key="leftLeg" className="h-[10px] w-[100px] bg-black absolute top-[230px] right-[-20px] rotate-[-60deg]" />, // Left Leg
+];
 
-const rightLeg = <div className="h-[10px] w-[100px] top-[230px] right-[-70px] border bg-black rotate-[60deg] absolute " />;
-
-const leftLeg = <div className="h-[10px] w-[100px] top-[230px] right-[-20px] border bg-black rotate-[-60deg] absolute " />;
-const HangmanDrawing = () => {
+const HangmanDrawing: React.FC<HangmanDrawingProps> = ({ incorrectGuesses }) => {
   return (
     <div className="relative">
+      {/* Hangman Stand */}
       <div className="h-[10px] w-[200px] bg-black ml-[120px]" />
-      {Head}
       <div className="h-[50px] w-[10px] bg-black ml-[120px] absolute top-0 right-0" />
-      {Body}
-      {rightArm}
-      {leftArm}
-      {rightLeg}
-      {leftLeg}
       <div className="h-[400px] w-[10px] bg-black ml-[120px]" />
-
       <div className="h-[10px] w-[250px] bg-black" />
+
+      {/* Reveal body parts gradually based on incorrect guesses */}
+      {bodyParts.slice(0, incorrectGuesses)}
     </div>
   );
 };
