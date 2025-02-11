751ecc59fd9cbd23de4215ddfd4f06389404d7e1
Parth Surnar <parth.surnar6@gmail.com>
2025-02-11 14:30:31 +0530

    Random words

--- 05_hangman-game/src/components/HangmanWord.tsx
+++ 05_hangman-game/src/components/HangmanWord.tsx
@@ -1,16 +1,22 @@
-const HangmanWord = () => {
-  const word = "hello";
-  const guessedLetters = ["h", "e", "l"];
+import React from "react";
+
+type HangmanWordProps = {
+  word: string;
+  guessedLetters: string[];
+};
+
+const HangmanWord: React.FC<HangmanWordProps> = ({ word, guessedLetters }) => {
   return (
-    <div className="flex justify-center items-center gap-4 text-8xl font-bold uppercase ">
+    <div className="flex justify-center items-center gap-4 text-8xl font-bold uppercase">
       {word.split("").map((letter, index) => (
-        <div key={index} className="border-b-4 border-black  text-center">
-        <span>{guessedLetters.includes(letter) ? letter : "_"} </span>
+        <div key={index} className="border-b-4 border-black text-center w-16">
+          <span className={guessedLetters.includes(letter) ? "visible" : "invisible"}>
+            {letter}
+          </span>
         </div>
       ))}
     </div>
+  );
+};
 
-  )
-}
-
-export default HangmanWord
+export default HangmanWord;
