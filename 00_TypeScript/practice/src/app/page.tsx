91e3587be3a94052fee412d5b93e516ae7d15c5b
Parth Surnar <parth.surnar6@gmail.com>
2025-02-13 23:23:50 +0530

    Contact page added

--- 00_TypeScript/practice/src/app/page.tsx
+++ 00_TypeScript/practice/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { useDisableRightClick } from "@/components/Disable";
+// import { useDisableRightClick } from "@/components/Disable";
 import Lenis from 'lenis'
 export default function Home() {
   // useEffect(() => {
@@ -10,7 +10,7 @@ export default function Home() {
   //     autoRaf: true,
   //   });
   // }, []);
-  useDisableRightClick();
+  // useDisableRightClick();
   return (
     <>
 
@@ -18,6 +18,7 @@ export default function Home() {
       <div className="min-h-screen flex flex-col items-center justify-center bg-slate-400 ">
         {/* <h1 className="font-semibold text-3xl ">First page</h1> */}
         < Link href="/about" className="font-semibold text-3xl armour ">About</Link>
+        < Link href="/contact" className="font-semibold text-3xl armour ">Contact</Link>
       </div>
       <div className="min-h-screen flex flex-col items-center justify-center bg-purple-400 ">Second Page</div>
       <div className="min-h-screen flex flex-col items-center justify-center bg-red-400 ">Third Page</div>
