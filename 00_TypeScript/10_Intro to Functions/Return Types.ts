3e618e5d68dfd7f2caf38e60aeb56241a727da32
Parth Surnar <parth.surnar6@gmail.com>
2025-02-15 14:41:08 +0530

    logmessage

--- 00_TypeScript/10_Intro to Functions/Return Types.ts
+++ 00_TypeScript/10_Intro to Functions/Return Types.ts
@@ -9,3 +9,23 @@ function add(a: number, b: number): number {
   
   console.log(add(5, 10)); // ✅ Output: 15
   
+
+// 🚀 Functions can return any type
+// ----------------------------------------------------
+// Functions can return any type, not just primitive types.
+// In this example, the function returns an object.
+function createUser(name: string, age: number): object {
+    return { name, age };
+  }
+  
+  console.log(createUser("John", 30)); // ✅ Output: { name: 'John', age: 30 }
+
+// 🚀 Functions can return void
+
+// Functions can return `void` when they do not return anything.
+// In this example, the function does not return anything.
+function logMessage1(message: string): void {
+    console.log(message);
+  }
+  
+  logMessage("This function returns nothing!"); // ✅ Output: This function returns nothing!
