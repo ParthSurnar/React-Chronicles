12d5eb8af3e1b06188f780a1e95fc40e0c0bc919
Parth Surnar <parth.surnar6@gmail.com>
2025-02-15 14:40:54 +0530

    multiply

--- 00_TypeScript/10_Intro to Functions/Rest Parameters.ts
+++ 00_TypeScript/10_Intro to Functions/Rest Parameters.ts
@@ -8,4 +8,17 @@ function sum(...numbers: number[]): number {
   }
   
   console.log(sum(1, 2, 3, 4, 5)); // ✅ Output: 15
+
+// Rest parameters allow you to accept multiple arguments as an array.
+// Rest parameters are denoted by `...` followed by the parameter name.
+// In the above example, `numbers` is a rest parameter that accepts multiple numbers.
+// The `sum` function returns the sum of all the numbers passed as arguments.
+
+// 🚀 Rest parameters with other parameters
+
+
+function multiply1(multiplier: number, ...numbers: number[]): number {
+    return numbers.reduce((acc, num) => acc * num, 1) * multiplier;
+  }
   
+  console.log(multiply1(2, 1, 2, 3, 4, 5)); // ✅ Output: 240
