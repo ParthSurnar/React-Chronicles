736801ac11efd942fb7df30173cf2e80e396e5b9
Parth Surnar <parth.surnar6@gmail.com>
2025-02-15 13:47:42 +0530

    Anonymous function practice

--- 00_TypeScript/10_Intro to Functions/Anonymous Functions.ts
+++ 00_TypeScript/10_Intro to Functions/Anonymous Functions.ts
@@ -8,4 +8,26 @@ const greetUser = function (name: string): string {
   };
   
   console.log(greetUser("Parth")); // ✅ Output: Hello, Parth!
+
+// 🚀 Anonymous functions can be assigned to variables
+// ----------------------------------------------------
+// Anonymous functions can be assigned to variables.
+const greetUser7 = function (name: string): string {
+    return `Hello, ${name}!`;
+  };
   
+  console.log(greetUser1("John"));
+  console.log(greetUser1("Doe"));
+
+// 🚀 Anonymous functions can be passed as argument
+// ----------------------------------------------------
+// Anonymous functions can be passed as arguments to other functions.
+function greetUser2(name: string, callback: (name: string) => string): string {
+    return callback(name);
+  }
+  
+  const greet7 = function (name: string): string {
+    return `Hello, ${name}!`;
+  };
+  
+  console.log(greetUser2("Parth", greet7)); // ✅ Output: Hello, Parth!
