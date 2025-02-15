2d0679d058c617f7e5a30476bfcec3812f5202fd
Parth Surnar <parth.surnar6@gmail.com>
2025-02-15 14:39:44 +0530

    summary

--- 00_TypeScript/10_Intro to Functions/Function Overloading.ts
+++ 00_TypeScript/10_Intro to Functions/Function Overloading.ts
@@ -11,3 +11,27 @@ function getData(value: number | string): string {
 
 console.log(getData(101)); // ✅ Output: Fetching data for: 101
 console.log(getData("Parth")); // ✅ Output: Fetching data for: Parth
+
+// 🚀 Error: Argument of type 'boolean' is not assignable to parameter of type 'string | number'
+// console.log(getData(true)); // ❌ Error
+
+// The `getData` function has two signatures:
+
+// 1. `getData(id: number): string`
+// 2. `getData(name: string): string`
+
+// The implementation of the function is the third signature:
+// `getData(value: number | string): string`
+// The function returns a string with the value passed to it.
+
+// When calling the function, TypeScript determines the correct signature based on the argument type.
+// The function implementation is the same for both signatures.
+// The function returns a string with the value passed to it.
+// The function can accept either a number or a string as an argument
+
+// example of error
+// getData(true); // ❌ Error
+ 
+
+// 📚 Learn More
+// https://www.typescriptlang.org/docs/handbook/2/functions.html#function-overloads
