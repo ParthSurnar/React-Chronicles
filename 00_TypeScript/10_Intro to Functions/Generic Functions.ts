4f86cd88c4aacbbdc3ea94910f9d1c50d28ebb49
Parth Surnar <parth.surnar6@gmail.com>
2025-02-15 14:40:03 +0530

    getLength

--- 00_TypeScript/10_Intro to Functions/Generic Functions.ts
+++ 00_TypeScript/10_Intro to Functions/Generic Functions.ts
@@ -9,4 +9,15 @@ function identity<T>(value: T): T {
   
   console.log(identity<number>(10)); // ✅ Output: 10
   console.log(identity<string>("Hello")); // ✅ Output: Hello
+
+// 🚀 Generic functions can have constraint
+// 
+// Constraints allow you to limit the types that can be passed to a generic function.
+// In this example, the function only accepts types that have a length property.
+function getLength<T extends { length: number }>(value: T): number {
+    return value.length;
+  }
   
+  console.log(getLength("Hello")); // ✅ Output: 5
+  console.log(getLength([1, 2, 3])); // ✅ Output: 3
+  console.log(getLength({ length: 10 })); // ✅ Output: 10
