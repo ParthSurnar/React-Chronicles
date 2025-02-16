c420253c370bbede4fbf9603200c8b5a21402ce1
Parth Surnar <parth.surnar6@gmail.com>
2025-02-16 20:13:22 +0530

    functions

--- 00_TypeScript/09_Classes & Objects/Class Method.ts
+++ 00_TypeScript/09_Classes & Objects/Class Method.ts
@@ -18,3 +18,54 @@ class User {
   const user1 = new User("Parth");
   user1.greet(); // Output: Hello, Parth!
   
+  // another example
+  class AnimalClass {
+    name: string;
+  
+    constructor(name: string) {
+      this.name = name;
+    }
+  
+    makeSound() {
+      console.log(`${this.name} makes a sound`);
+    }
+  }
+
+  const dogInstance = new AnimalClass("Dog");
+  dogInstance.makeSound(); // Output: Dog makes a sound
+  // ===========================
+  // 🔹 3️⃣ CLASS INHERITANCE
+  // ===========================
+  // Inheritance allows a class to inherit properties and methods from another class.
+
+  class PersonClass {
+    name: string;
+    age: number;
+  
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    greet() {
+      console.log(`Hello, ${this.name}!`);
+    }
+  }
+
+  class Student extends PersonClass {
+    rollNumber: number;
+  
+    constructor(name: string, age: number, rollNumber: number) {
+      super(name, age);
+      this.rollNumber = rollNumber;
+    }
+  
+    study() {
+      console.log(`${this.name} is studying`);
+    }
+  }
+
+  const student1 = new Student("Alice", 20, 1234);
+  student1.greet(); // Output: Hello, Alice!
+  student1.study(); // Output: Alice is studying
+  
