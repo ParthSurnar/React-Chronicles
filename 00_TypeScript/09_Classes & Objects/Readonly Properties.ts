e391090e12412aacc4621d63fed4a202e4af35c1
Parth Surnar <parth.surnar6@gmail.com>
2025-02-17 23:51:35 +0530

    another way to declare readonly properties

--- 00_TypeScript/09_Classes & Objects/Readonly Properties.ts
+++ 00_TypeScript/09_Classes & Objects/Readonly Properties.ts
@@ -17,3 +17,16 @@ class Product {
   console.log(product1.productId); // Output: 12345
   // product1.productId = "67890"; // ❌ ERROR: Cannot assign to 'productId' because it is a read-only property.
   
+
+// In the above example, the `productId` property is marked as `readonly`.
+// The `productId` property is initialized in the constructor.
+
+// another way to declare readonly properties
+class UserWithReadonly {
+    constructor(readonly userId: string, public name: string) {}
+  }
+  
+  const userReadonly = new UserWithReadonly("U123", "John");
+
+// In the above example, the `userId` property is marked as `readonly` using the `readonly` keyword in the constructor parameter.
+// The `userId` property is initialized in the constructor.
