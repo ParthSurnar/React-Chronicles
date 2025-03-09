6579fb7960c184042728ae3c190e349722784b55
Parth Surnar <parth.surnar6@gmail.com>
2025-03-09 22:21:41 +0530

    change

--- 00_TypeScript/practice/src/app/extra/page.tsx
+++ 00_TypeScript/practice/src/app/extra/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
       <h1 className="text-3xl font-bold mb-4">Extra Page</h1>
       <div className="bg-white p-6 rounded-lg shadow-md">
       <p className="text-gray-700">
-        Extra page content with enhanced styling
+        Extra page content 
       </p>
       <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
         Click 
