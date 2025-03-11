c6149ece8a11cb49193e49df0c646dfa37f2c0b3
Parth Surnar <parth.surnar6@gmail.com>
2025-03-11 22:12:41 +0530

    ts

--- 00_TypeScript/practice/src/app/extra/page.tsx
+++ 00_TypeScript/practice/src/app/extra/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
         Brokennnnnn
       </p>
       <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-        Click 
+        Click me
       </button>
       </div>
     </div>
