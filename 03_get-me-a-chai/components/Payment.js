bbdbb85d30da61359f9b928934be368c8fb83d8d
Parth Surnar <parth.surnar6@gmail.com>
2025-03-22 09:45:29 +0530

    change

--- 03_get-me-a-chai/components/Payment.js
+++ 03_get-me-a-chai/components/Payment.js
@@ -185,7 +185,7 @@ const Payment = ({ username }) => {
                         <div className='flex flex-col md:flex-row gap-2 mt-5'>
                             <button className='bg-slate-800 p-3 rounded-lg' onClick={() => pay(1000)}>Pay ₹10</button>
                             <button className='bg-slate-800 p-3 rounded-lg' onClick={() => pay(2000)}>Pay ₹40</button>
-                            <button className='bg-slate-800 p-3 rounded-lg' onClick={() => pay(3000)}>Pay ₹30</button>
+                            <button className='bg-slate-800 p-3 rounded-lg' onClick={() => pay(3000)}>Pay ₹60</button>
                         </div>
                     </div>
                 </div>
