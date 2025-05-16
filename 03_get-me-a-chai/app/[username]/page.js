493024c70be19547f4dd3615f0d76be4bb8fb6fe
Parth Surnar <parth.surnar6@gmail.com>
2025-05-16 21:32:03 +0530

    changes

--- 03_get-me-a-chai/app/[username]/page.js
+++ 03_get-me-a-chai/app/[username]/page.js
@@ -63,7 +63,7 @@ const Username = ({ params: paramsPromise }) => {
                         <ul className="my-3 ">
                             <li className="my-2 hover:bg-gray-800 transition-all duration-300 p-1 gap-2 rounded-lg flex">
                                 <img src="avatar.gif" width={45} height={45} alt="" />
-                                Rohan donated $32 with a message "Keep up the great work! 🚀"</li>
+                                Rohan donated $32 with a message "Keep up the great work!🚀"</li>
                             <li className="my-2 hover:bg-gray-800 transition-all duration-300 p-1 gap-2 flex rounded-lg">
                                 <img src="avatar.gif" width={45} height={45} alt="" />
                                 Ayesha donated $50 with a message "Proud to support this!💜"</li>
