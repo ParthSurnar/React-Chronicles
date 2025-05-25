5eb7a165ae3f50a91f4de3c9c06039372cf73021
Parth Surnar <parth.surnar6@gmail.com>
2025-05-25 12:43:44 +0530

    change

--- 03_get-me-a-chai/app/login/page.js
+++ 03_get-me-a-chai/app/login/page.js
@@ -18,7 +18,7 @@ const Login = () => {
         <>
             <div className='font-nosifer text-soft-neon flex justify-center items-center pt-7 text-center container mx-auto flex-col'>
                 <h1 className='text-4xl  mx-auto'>
-                    L0GIN TO GET YOUR FANS SUPPORt YOU
+                    L0GIN TO GET YOUR FANS SUPPORT YOU
                 </h1>
 
                 <div className="flex flex-col gap-2 [] p-10">
